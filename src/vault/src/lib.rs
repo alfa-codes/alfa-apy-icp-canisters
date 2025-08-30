c8b82cb011bd1dc7e40e5e5bd104d5baf6277cbb
v7m <v7m@example.com>
2025-08-30 18:53:07 +0400

    Update icrc28_trusted_origins at vault

--- src/vault/src/lib.rs
+++ src/vault/src/lib.rs
@@ -230,6 +230,7 @@ fn icrc28_trusted_origins() -> Icrc28TrustedOriginsResponse {
     let trusted_origins = vec![
         String::from("https://47r3x-paaaa-aaaao-qj6ha-cai.icp0.io"),
         String::from("https://kxsds-zqaaa-aaaai-atk3a-cai.icp0.io"),
+        String::from("https://5ac7m-miaaa-aaaah-qqfgq-cai.icp0.io"),
     ];
 
     Icrc28TrustedOriginsResponse { trusted_origins }
